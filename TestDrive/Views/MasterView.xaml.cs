204cd1aee71327ea173125c769a25801bae5bdca
leandro candido <leandrocandido@MacBook-Pro-de-leandro.local>
2017-09-10 20:55:41 -0300

    alterando e corrigindo problemas na master detail page

---
+++ TestDrive/Views/MasterView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TestDrive.Views
+{
+    public partial class MasterView : ContentPage
+    {
+        public MasterView()
+        {
+            InitializeComponent();
+        }
+    }
+}
